ee7e954d0a5e2e17e851854f7570df3356ccf6d9
Serafin Wesnidge <leaf466@gmail.com>
2019-08-13 12:59:30 -0700

    feat: Toaster dynamically resizes
    Toaster border resizes to minimum width
    based on text content and handles window resize
    dynamically as well

--- static/toaster.js
+++ static/toaster.js
@@ -11,9 +11,17 @@ class Toaster {
     this._messageEnd = this._messageEnd.bind(this)
     this._handleQueue = this._handleQueue.bind(this)
     this._handleMessage = this._handleMessage.bind(this)
+    this._recalculateSize = this._recalculateSize.bind(this)
+
+    // Add dynamic sizing
+    document.head.appendChild(document.createElement('style'))
+    this._styleSheet = document.styleSheets[document.styleSheets.length - 1]
+    this._recalculateSize()
+    window.addEventListener('resize', this._recalculateSize)
   }
 
   pushMessage({ type, payload }) {
+    // Handle Interrupt
     if(payload.interrupt) {
       clearTimeout(this._msgTimeout)
       this.messageBeingDisplayed = false
@@ -33,6 +41,7 @@ class Toaster {
         this._handleMessage({ type, payload })
       })
     } else {
+      // Normal Append
       this._messageQueue.push(() => {
         this._handleMessage({ type, payload })
       })
@@ -40,20 +49,54 @@ class Toaster {
     this._handleQueue()
   }
 
+  _recalculateSize() {
+    const { width, height } = getComputedStyle(this._toaster)
+    const computedHalfWidth = Number(width.replace('px', '')) / 2
+    const computedHeight = Number(height.replace('px', '')) + 20
+    while(this._styleSheet.cssRules.length > 0) {
+      this._styleSheet.deleteRule(0)
+    }
+    this._styleSheet.insertRule(`
+      .toaster {
+        left: calc(50vw - ${computedHalfWidth}px);
+      }
+    `, 0)
+    this._styleSheet.insertRule(`
+      .toaster-shown {
+        top: calc(100vh - ${computedHeight}px);
+      }
+    `, 1)
+    this._styleSheet.insertRule(`
+      @keyframes slidein {
+        from { top: 100vh; }
+        to { top: calc(100vh - ${computedHeight}px); }
+      }
+    `, 2)
+    this._styleSheet.insertRule(`
+      @keyframes slideout {
+        from { top: calc(100vh - ${computedHeight}px); }
+        to { top: 100vh; }
+      }
+    `, 3)
+  }
+
   _handleMessage({ type, payload }) {
     this.messageBeingDisplayed = true
     console.log('type: ', type)
     const sCase = {
       error: () => {
-        this._toasterText.classList.add('warning')
+        this._toaster.classList.add('warning')
         this._toasterText.innerText = `Error: ${payload.error}`
+        this._recalculateSize()
       },
       notification: () => {
-        this._toasterText.classList.remove('warning')
+        this._toaster.classList.remove('warning')
         this._toasterText.innerText = payload.message
+        this._recalculateSize()
       },
       default: () => {
         this._toasterText.innerText = ''
+        this._recalculateSize()
         throw new Error('Error - invalid toaster message case')
       },
     }
