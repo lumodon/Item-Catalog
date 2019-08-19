705bf185e87854eeb141fbe88e25288d8cf7e2f0
Serafin Wesnidge <leaf466@gmail.com>
2019-08-19 12:43:04 -0700

    chore: Remove unused logs

--- routes/GConnect.py
+++ routes/GConnect.py
@@ -35,11 +35,8 @@ def ShowLogin():
 # Citation: https://github.com/udacity/ud330/blob/master/Lesson2/step5/project.py
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    print 'Running gconnect'
     output = '<h1>Done</h1>'
     passed_state = request.args.get('state')
-    print 'state: '
-    print passed_state
     # Validate state token
     if passed_state != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -47,43 +44,30 @@ def gconnect():
         return response
     # Obtain authorization code
     authcode = request.data
-    print 'code:'
-    print authcode
 
-    print 'oauth flow'
     try:
         # Upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-        print oauth_flow
         oauth_flow.redirect_uri = 'postmessage'
-        print 'oauthflow'
-        print oauth_flow.redirect_uri
         credentials = oauth_flow.step2_exchange(authcode)
-        print 'credentials:'
-        print credentials
     except FlowExchangeError:
         response = make_response(
             json.dumps('Failed to upgrade the authorization code.'), 401)
-        print response
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    print 'access token validation'
     # Check that the access token is valid.
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    print 'access token result:'
-    print result
     # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    print 'checking access token intended user'
     # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
@@ -92,16 +76,13 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    print 'access token valid for this app'
     # Verify that the access token is valid for this app.
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    print 'debug 2'
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
@@ -110,24 +91,20 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    print 'debug 3'
     # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
-    print 'debug 4'
     # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
-    print 'debug 5'
 
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-    print 'debug 6'
 
     output = ''
     output += '<h1>Welcome, '
@@ -136,9 +113,7 @@ def gconnect():
     output += '<img src="'
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-    print 'debug 7'
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
     return output
 
 
@@ -172,8 +147,3 @@ def gdisconnect():
         response = make_response(json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
-
-
-@app.route('/logout')
-def LogOut():
-    return render_template('logout.html', CLIENT_ID=CLIENT_ID)
