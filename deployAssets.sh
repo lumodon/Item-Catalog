c4dbe03a3bb3d4965a47bacb9b514b6b300e896c
Serafin Wesnidge <leaf466@gmail.com>
2019-08-25 20:37:15 -0700

    fix: Fixes deployment script

--- deployAssets.sh
+++ deployAssets.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rsync -r ./.env ./assets/.env
-rsync -r ./static/favicon.ico ./assets/favicon.ico
+rsync -r ./.env assets/.env
+rsync -r ./client_secrets.json assets/client_secrets.json
+rsync -r ./static/favicon.ico assets/favicon.ico
 tar -czvf ./syncs/assets.tar.gz ./assets
 gdrive sync upload syncs --keep-local 1uBMXceRugdm_Zzg4onemoq7cuLHB9NWL
